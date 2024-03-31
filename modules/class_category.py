aefba2b6775fe02a47b6c02de85d821304eb7389
Вячеслав Лукин <bafana.slv@gmail.com>
2024-03-31 09:58:49 +0300

    Устранение замечаний наставника.

--- modules/class_category.py
+++ modules/class_category.py
@@ -32,26 +32,24 @@ class Category:
             prod_list += str(prod)+'\n'
         return prod_list
 
-    def add_product(self, dict_new_product):
+    def add_product(self, new_product):
         """Метод предназначен для добавление нового товара в категорию. Если наимнования товара совпадает с имеющимся
         товаром, то увеличивается количество и при необходимости корректируется цена товара."""
         # dict_new_product - словарь с атрибутами нового товара.
-        found_product = self.search_product(dict_new_product["name"], self.__products)
+        found_product = self.search_product(new_product.name, self.__products)
         if found_product is None:
-            # если товар не нашелся в списке, то с помощью класса Product получаем новый экземпляр и добавляем его
-            # в список товаров текущей категории.
-            new_product = Product.new_product(dict_new_product)
+            # если товар не нашелся в списке, то добавляем его в список товаров текущей категории.
             if not isinstance(new_product, Product):
                 raise TypeError("Добавлять можно только объекты одного класса или его наследников.")
             self.__products.append(new_product)
             Category.product_names_quantity += 1
         else:
             # если товар нашелся, то увеличиваем его количество и, при необходимости, корректируем цену.
-            found_product.quantity += dict_new_product["quantity"]
-            if dict_new_product["price"] != found_product.product_price:
-                if dict_new_product["price"] < found_product.product_price:
+            found_product.quantity += new_product.quantity
+            if new_product.product_price != found_product.product_price:
+                if new_product.product_price < found_product.product_price:
                     if input(f'Вы согласны на понижение цены товара "{found_product.name}" ?\n') == 'y':
-                        found_product.product_price = dict_new_product["price"]
+                        found_product.product_price = new_product.product_price
 
     @staticmethod
     def search_product(prod_name, products):
