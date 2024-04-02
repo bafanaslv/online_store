8820e47447ae63e171a3e11c3d31ceae62f8dc57
Вячеслав Лукин <bafana.slv@gmail.com>
2024-04-02 20:24:56 +0300

    Добавление теста на добавление продукта из другой категории.

--- modules/class_order.py
+++ modules/class_order.py
@@ -4,11 +4,11 @@ from abstract_classes import Categoryabc
 class Order(Categoryabc):
     """ Класс предназначен для описания покупки товара определенной категории и расчета остатка товара после покупки."""
     def __init__(self, product):
-        self.product = product
+        self.__product = product
         self.amount = product.product_price * product.quantity
 
     def __str__(self):
-        return f'Куплен товар: {self.product.name}, {self.product.quantity} шт. на сумму - {self.amount}'
+        return f'Куплен товар: {self.__product.name}, {self.__product.quantity} шт. на сумму - {self.amount}'
 
     def __len__(self):
         pass
